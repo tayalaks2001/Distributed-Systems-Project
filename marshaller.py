2fee9b0a80c782477c2b827c790a5c2fd827171d
Aks <tayalaks2001@gmail.com>
2022-03-14 20:26:14 +0800

    Update marshaller.py

--- marshaller.py
+++ marshaller.py
@@ -1,24 +1,48 @@
 import sys
 import json
+import struct
+from marshalable import Marshalable
 
+class Marshaller:
 
-class BankAccount:
+	@staticmethod
+	def marshal(message, numBytes = 8):
 
-	def __init__(self, acctNumber, name, amount):
-		self.acctNumber = acctNumber
-		self.name = name
-		self.amount = amount
+		res = bytes()
 
-	def serialize(self):
-		fieldsDict = {"acctNumber": self.acctNumber, "name": self.name, "amount": self.amount}
-		fieldsJSON = json.dumps(fieldsDict)
-		return fieldsJSON
+		if (type(message) == str):
+			return Marshaller.marshal_string(message)
+		if (type(message) == int):
+			return Marshaller.marshal_int(message, numBytes)
+		if (type(message) == float):
+			return Marshaller.marshal_float(message)
+		
+		return TypeError
+		
+	
+	@staticmethod
+	def marshal_string(message):
+		return message.encode('utf-8')
+	
+	@staticmethod
+	def marshal_int(message, numBytes):
+		return message.to_bytes(numBytes, byteorder = 'big', signed = False)
+	
+	@staticmethod
+	def marshal_float(message, numBytes):
+		# return bytes(struct.pack('f', message))
 
+		intVal = int(message)
+		floatVal = message - intVal
+
+		result = bytes()
+		result += Marshaller.marshal_int(intVal, )
+	
 
 
 class Message:
 
-	def __init__(self, messageID, messageContent, account: BankAccount):
+	def __init__(self, messageID, messageContent, account):
 		self.messageID = messageID
 		self.messageContent = messageContent
 		self.messageLength = len(messageContent)
@@ -40,7 +64,6 @@ class Message:
 
 		
 		# encode message content
-		# endianness depends on interpreter
 		message = message.encode('utf-8')
 
 		
