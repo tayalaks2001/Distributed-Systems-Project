73ee05899e43b7c6ab99f78b0dc911201b6cc7ab
Aks <tayalaks2001@gmail.com>
2022-03-28 18:24:55 +0800

    currency_type metaclass bug fixing

--- currency_type.py
+++ currency_type.py
@@ -1,7 +1,14 @@
-from enum import Enum
+from enum import Enum, EnumMeta
 from messages.marshalable import Marshalable
+from messages.marshalable import ABCRegistryMeta
+from six import with_metaclass
 
-class CurrencyType(Enum, Marshalable):
+
+class TempMeta(EnumMeta, ABCRegistryMeta):
+    pass
+
+class CurrencyType(Enum, Marshalable, metaclass = TempMeta):
+
     """Enum to keep track of currency type of an account"""
     SGD = 1
     USD = 2
