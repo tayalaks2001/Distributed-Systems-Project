250d0f7b9a21c3ecaa8ad6994c8ab53e4a376a66
Aks <tayalaks2001@gmail.com>
2022-03-29 15:06:44 +0800

    Added currency conversion to currency_type

--- currency_type.py
+++ currency_type.py
@@ -11,8 +11,35 @@ class CurrencyType(Enum):
     """Enum to keep track of currency type of an account"""
     SGD = 1
     USD = 2
-    INR = 3 
+    INR = 3
+
+    __conversion_tbl__ ={
+                            "SGD":{
+                                    "INR": 55.83, 
+                                    "USD": 0.74
+                                    },
+                            "USD":{
+                                    "SGD": 1.36,
+                                    "INR": 75.94
+
+                            }, 
+                            "INR":{
+                                    "SGD": 0.018,
+                                    "USD": 0.013
+                            }
+                        }
 
     @staticmethod
     def object_type():
         return 10
+    
+    
+
+def convert(currency_from: CurrencyType, currency_to: CurrencyType, value: int):
+
+    if currency_from == currency_to:
+        return value
+
+    multiplying_factor = CurrencyType.__conversion_tbl__[currency_from.name][currency_to.name]
+    
+    return value * multiplying_factor
