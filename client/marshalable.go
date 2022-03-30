bf46a802b2a4f546a045246acc733d86322097d3
Sdidt <siddharthms607@gmail.com>
2022-03-30 21:16:37 +0800

    Removed extra comments

--- client/marshalable.go
+++ client/marshalable.go
@@ -1,5 +1,3 @@
-//adapted from : https://github.com/AlexsJones/go-type-registry
-
 package main
 
 import (
@@ -16,23 +14,19 @@ type Marshalable interface {
 	get_field_types() map[int]reflect.Type
 }
 
-//Entry is a utility structure for holding type information for reflection
 type Entry struct {
 	Name    string
 	RefType reflect.Type
 }
 
-//Unwrap brings the entry back into a real object
 func (e *Entry) Unwrap() interface{} {
 	return reflect.New(e.RefType).Elem().Interface()
 }
 
-//Registry is a utility structure for holding type information for reflection
 type Registry struct {
 	storedEntries map[int]*Entry
 }
 
-//Put will register a type against an int
 func (r *Registry) Put(m Marshalable) {
 	var object_type int = m.object_type()
 	t := reflect.TypeOf(m)
@@ -42,7 +36,6 @@ func (r *Registry) Put(m Marshalable) {
 	r.storedEntries[object_type] = e
 }
 
-//Get will return a new instance of type if avialable
 func (r *Registry) Get(object_type int) (*Entry, error) {
 	e := r.storedEntries[object_type]
 	if e == nil {
@@ -51,7 +44,6 @@ func (r *Registry) Get(object_type int) (*Entry, error) {
 	return e, nil
 }
 
-//NewRegistry takes optional functions that can manipulate the object initialised
 func NewRegistry(options ...func(*Registry) error) (*Registry, error) {
 	r := &Registry{storedEntries: make(map[int]*Entry)}
 	for _, op := range options {
@@ -66,7 +58,6 @@ func NewRegistry(options ...func(*Registry) error) (*Registry, error) {
 func getMarshalableObject(unwrapped_val interface{}) Marshalable {
 	var object Marshalable = nil
 	switch unwrapped_val.(type) {
-		//The type is a pointer to the foo object
 		case *CloseAccountMessage:
 			object = CloseAccountMessage{}
 		case *CloseAccountResponse:
